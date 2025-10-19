9e7eaf97afec46b2d89d0cdbc090c0333e7ec104
Mikhail Rudakov (Work) <miirld0302@gmail.com>
2025-10-19 23:25:38 +0300

    Task 3.14

--- frontend/src/layouts/AppLayout.vue
+++ frontend/src/layouts/AppLayout.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>
-    <app-header />
+  <component :is="layout">
     <slot />
-  </div>
+  </component>
 </template>
 
 <script setup>
-import AppHeader from "./AppHeader.vue";
+import { shallowRef, watch } from "vue";
+import { useRoute } from "vue-router";
+import AppLayoutDefault from "@/layouts/DefaultLayout.vue";
+
+const route = useRoute();
+const layout = shallowRef(null);
+
+watch(
+  () => route.meta,
+  async (meta) => {
+    try {
+      const component = await import(`./${meta.layout}.vue`);
+      layout.value = component?.default || AppLayoutDefault;
+    } catch (e) {
+      layout.value = AppLayoutDefault;
+    }
+  },
+);
 </script>
